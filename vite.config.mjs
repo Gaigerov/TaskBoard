8ad512b45ebdfc3e6e97cfcb0b57554dedf91250
Mikhail Gaigerov <gaigerov@gmail.com>
2025-03-01 19:28:59 +0300

    vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,35 +1,36 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [
-    react(), 
-    svgr({ 
-      svgrOptions: {
-      },
-    }),
-],
-  server: {
-    port: 5173, 
-    open: true, 
-    cors: true,
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: true,
-    minify: 'esbuild',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+    base: "/TaskBoard/",
+    plugins: [
+        react(),
+        svgr({
+            svgrOptions: {
+            },
+        }),
+    ],
+    server: {
+        port: 5173,
+        open: true,
+        cors: true,
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: './src/config/App.css',
-      },
+    build: {
+        outDir: 'build',
+        sourcemap: true,
+        minify: 'esbuild',
     },
-  },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: './src/config/App.css',
+            },
+        },
+    },
 });
